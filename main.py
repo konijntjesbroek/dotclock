8d32da88074713ca0beeb7ec6ed7898d4fe520e6
Arlo Gittings <>
2021-04-29 10:57:08 +0000

    concatenating the lines using cr/lf was causing a weird artifact at the top of the range. Reverted to individual writes.

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ def demo(screen):
         line_1 = (secs % 12 *  marker[(secs // 12 + 1)] + (12 - (secs % 12)) * marker[secs // 12])
         line_2 = (mins % 12 *  marker[(mins // 12 + 1)] + (12 - (mins % 12)) * marker[mins // 12]) 
         line_3 = (hours % 12 * hmark[(hours // 12 + 1)] + (12 - (hours % 12)) * hmark[hours // 12]) 
-        clock_show = line_1 + '\n\r' + line_2 + '\n\r' + line_3
+        # clock_show = line_1 + '\n\r' + line_2 + '\n\r' + line_3
         
-        screen.print_at(clock_show, 0, 0)
+        screen.print_at(line_1, 0, 0)
+        screen.print_at(line_2, 0, 1)
+        screen.print_at(line_3, 0, 2)
         screen.refresh()
         time.sleep(1)
         
