ed2fef997065915f43a66ce35a0bdf8efe0f1eb1
Arlo Gittings <proticon@gmail.com>
2021-04-29 07:15:13 -0500

    Now uses color and a single charcter

--- main.py
+++ main.py
@@ -4,16 +4,24 @@ from asciimatics.screen import Screen
 def demo(screen):
     while(True):
         secs, mins, hours =[int (i) for i in time.strftime("%S %M %H", time.localtime()).split()]
-        marker=(' ', '.',':', '\'', '*', '#')
-        hmark =(' ', ':', '#')
-        line_1 = (secs % 12 *  marker[(secs // 12 + 1)] + (12 - (secs % 12)) * marker[secs // 12])
-        line_2 = (mins % 12 *  marker[(mins // 12 + 1)] + (12 - (mins % 12)) * marker[mins // 12]) 
-        line_3 = (hours % 12 * hmark[(hours // 12 + 1)] + (12 - (hours % 12)) * hmark[hours // 12]) 
+        mark = '#'
+        hmark = (' ', ':', '#')
+        shades = (0, 22, 28, 34, 40) 
+        line_1 = (secs // 5) * mark
+        line_2 = (mins // 5) * mark
+        line_3 = (hours % 12) * mark
+        am_line = (12 - (hours % 12)) * mark
         # clock_show = line_1 + '\n\r' + line_2 + '\n\r' + line_3
-        
-        screen.print_at(line_1, 0, 0)
-        screen.print_at(line_2, 0, 1)
-        screen.print_at(line_3, 0, 2)
+        screen.clear_buffer(255, 0, 0) 
+        screen.print_at(line_1, 0, 0, 46)
+        screen.print_at(mark, (secs // 5), 0, shades[secs % 5])
+        screen.print_at(line_2, 0, 1, 46)
+        screen.print_at(mark, (mins // 5), 1, shades[mins % 5])
+        if ( hours <= 12):
+            screen.print_at(line_3, 0, 2, shades[2])
+        else:
+            screen.print_at(line_3, 0, 2, 46)
+            screen.print_at(am_line, hours % 12, 2, shades[2])
         screen.refresh()
         time.sleep(1)
         
