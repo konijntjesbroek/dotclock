b413d9bccb7650d1bf9108897da6af3bb30e901c
Arlo Gittings <proticon@gmail.com>
2021-04-29 08:28:08 -0500

    Update main.py
    Cleanup

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def demo(screen):
     while(True):
         secs, mins, hours =[int (i) for i in time.strftime("%S %M %H", time.localtime()).split()]
         mark = '#'
-        shades = (0, 88, 203, 216, 222, 230)#20, 30, 83, 187, 208, 196) 
+        shades = (0, 88, 203, 216, 222, 230)
         line_1 = (secs // 5) * mark
         line_2 = (mins // 5) * mark
         line_3 = (hours % 12) * mark
