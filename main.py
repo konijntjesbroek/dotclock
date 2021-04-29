bb248d25b547b9a447b3bf7851e898cbdb4f58d8
Arlo Gittings <>
2021-04-29 10:50:57 +0000

    fixed the flicker

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import time
 from asciimatics.screen import Screen
 
 def demo(screen):
-    secs, mins, hours =[int (i) for i in time.strftime("%S %M %H", time.localtime()).split()]
-    marker=(' ', '.',':', '\'', '*', '#')
-    hmark =(' ', ':', '#')
-    line_1 = (secs % 12 *  marker[(secs // 12 + 1)] + (12 - (secs % 12)) * marker[secs // 12])
-    line_2 = (mins % 12 *  marker[(mins // 12 + 1)] + (12 - (mins % 12)) * marker[mins // 12]) 
-    line_3 = (hours % 12 * hmark[(hours // 12 + 1)] + (12 - (hours % 12)) * hmark[hours // 12]) 
-    clock_show = line_1 + '\n\r' + line_2 + '\n\r' + line_3
-    
-    screen.print_at(clock_show, 0, 0)
-    screen.refresh()
-    time.sleep(1)
-    
-while(True):    
-    Screen.wrapper(demo)
+    while(True):
+        secs, mins, hours =[int (i) for i in time.strftime("%S %M %H", time.localtime()).split()]
+        marker=(' ', '.',':', '\'', '*', '#')
+        hmark =(' ', ':', '#')
+        line_1 = (secs % 12 *  marker[(secs // 12 + 1)] + (12 - (secs % 12)) * marker[secs // 12])
+        line_2 = (mins % 12 *  marker[(mins // 12 + 1)] + (12 - (mins % 12)) * marker[mins // 12]) 
+        line_3 = (hours % 12 * hmark[(hours // 12 + 1)] + (12 - (hours % 12)) * hmark[hours // 12]) 
+        clock_show = line_1 + '\n\r' + line_2 + '\n\r' + line_3
+        
+        screen.print_at(clock_show, 0, 0)
+        screen.refresh()
+        time.sleep(1)
+        
+Screen.wrapper(demo)
